687270e33be3f74f5c49168e79239c6b7fec8ca1
Mate Meszaros <mate8904@gmail.com>
2016-05-04 20:47:08 +0200

    CameraHelper javadoc

--- core/src/com/canyonbunny/game/util/CameraHelper.java
+++ core/src/com/canyonbunny/game/util/CameraHelper.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.canyonbunny.game.gameobject.AbstractGameObject;
 
+/**
+ * A kamera pozícionálását és a zoomolást végző segédosztály.
+ */
 public class CameraHelper {
     private static final String TAG = CameraHelper.class.getName();
 
@@ -20,6 +23,10 @@ public class CameraHelper {
         zoom = 1.0f;
     }
 
+    /**
+     * Képkockánként mozgatja a kamerát.
+     * @param deltaTime A frissítések között eltelt idő
+     */
     public void update (float deltaTime) {
         if (!hasTarget()) return;
 
@@ -29,34 +36,71 @@ public class CameraHelper {
         position.y = Math.max(-1f, position.y);
     }
 
+    /**
+     * Pozíció setter metódus.
+     * @param x X koordináta
+     * @param y Y koordináta
+     */
     public void setPosition (float x, float y) {
         this.position.set(x, y);
     }
 
+    /**
+     * Pozíció getter metódus.
+     * @return A pozíció
+     */
     public Vector2 getPosition () { return position; }
 
+    /**
+     * Zoomolás növelése.
+     * @param amount Mennyiség, amivel növelni kell a zoomot
+     */
     public void addZoom (float amount) { setZoom(zoom + amount); }
 
+    /**
+     * Zoom setter metódus, lekorlátozva a min-max intervallumra.
+     * @param zoom A beállítandó zoom érték
+     */
     public void setZoom (float zoom) {
         this.zoom = MathUtils.clamp(zoom, MAX_ZOOM_IN, MAX_ZOOM_OUT);
     }
 
+    /**
+     * Zoom getter metódus.
+     * @return A zoom jelenlegi értéke
+     */
     public float getZoom () { return zoom; }
 
+    /**
+     * Kamera célpont beállítása.
+     * @param target A célpont objektum
+     */
     public void setTarget (AbstractGameObject target) {
         this.target = target;
     }
 
+    /**
+     * Célpont getter metódus.
+     * @return A célpont objektum
+     */
     public AbstractGameObject getTarget () {
         return target;
     }
 
+    /**
+     * Van-e célpont ellenőrzés?
+     * @return Van beállítva célpont?
+     */
     public boolean hasTarget () { return target != null; }
 
     public boolean hasTarget (AbstractGameObject target) {
         return hasTarget() && this.target.equals(target);
     }
 
+    /**
+     * A beállítások alkalmazása egy kamerára.
+     * @param camera A beállítandó kamera
+     */
     public void applyTo (OrthographicCamera camera) {
         camera.position.x = position.x;
         camera.position.y = position.y;
