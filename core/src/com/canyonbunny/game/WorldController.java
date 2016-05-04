a79f3c9af2dfb635de4bed1868610d13fad975e7
Mate Meszaros <mate8904@gmail.com>
2016-05-04 18:52:42 +0200

    WorldController javadoc

--- core/src/com/canyonbunny/game/WorldController.java
+++ core/src/com/canyonbunny/game/WorldController.java
@@ -9,7 +9,10 @@ import com.badlogic.gdx.math.Rectangle;
 import com.canyonbunny.game.screen.MenuScreen;
 import com.canyonbunny.game.util.*;
 
-//implement InputHandler for interface - to force implementing all methods
+/**
+ * A fő kontroller osztály, inicilaizálja a játékot, kezeli az inputot és az ütközési eseményeket.
+ * Nyilvántartja a pontok és az életek számát.
+ */
 public class WorldController extends InputAdapter {
     private static final String TAG = WorldController.class.getName();
 
@@ -26,6 +29,9 @@ public class WorldController extends InputAdapter {
         init();
     }
 
+    /**
+     * Inicializáló metódus, beállítja a kezdeti változóértékeket, meghívja a pálya inicializálást.
+     */
     private void init () {
         //libgdx needs to be told where its going to receive input events
         Gdx.input.setInputProcessor(this);
@@ -34,10 +40,17 @@ public class WorldController extends InputAdapter {
         initLevel();
     }
 
+    /**
+     * A szintet inicializáló metódus, kinullázza a ponzokat is.
+     */
     private void initLevel () {
         score = 0;
     }
 
+    /**
+     * Minden frissítéskor meghívódó metódus, ha vége a játéknak, kilép a menübe. Meghívja az inputkezelést
+     * @param deltaTime A frissítések között eltelt idő
+     */
     public void update (float deltaTime) {
         handleDebugInput(deltaTime);
 
@@ -51,10 +64,20 @@ public class WorldController extends InputAdapter {
 
     //input
     //input handling method 1 - query every frame
+
+    /**
+     * Debug bemenetet kezelő metódus.
+     * @param deltaTime Frissítések között eltelt idő az update() metódusból
+     */
     private void handleDebugInput (float deltaTime) {
         if (Gdx.app.getType() != ApplicationType.Desktop) return;
     }
 
+    /**
+     * A kamerát elmozdító metódus.
+     * @param x Kamera x koordináta
+     * @param y Kamera y koordináta
+     */
     private void moveCamera (float x, float y) {
         x += cameraHelper.getPosition().x;
         y += cameraHelper.getPosition().y;
@@ -62,6 +85,12 @@ public class WorldController extends InputAdapter {
     }
 
     //input handling method 2 - this is a "callback"
+
+    /**
+     * Keyboard bemenetet észlelő metódus (billentyű felengedése).
+     * @param keycode A felengedett billentyű kódja
+     * @return Ha nem tudja értelmezni a felengedett billentyűt, akkor false
+     */
     @Override
     public boolean keyUp (int keycode) {
         // Reset game world
@@ -83,12 +112,21 @@ public class WorldController extends InputAdapter {
     private Rectangle r2 = new Rectangle();
 
     // CanyonBunnyGame over
+
+    /**
+     * Játék vége, ha az életek száma <= 0.
+     * @return Vége van-e a játéknak?
+     */
     public boolean isGameOver () {
         return lives <= 0;
     }
 
 
     // Menu
+    
+    /**
+     * Visszalép a főmenübe.
+     */
     private void backToMenu () {
         // switch to menu screen
         game.setScreen(new MenuScreen(game));
